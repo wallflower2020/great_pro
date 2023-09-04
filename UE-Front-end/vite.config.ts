6b2c59a271d43220fba0a8fee4356e6ce47ba727
wallflower2020 <mr.bes@qq.com>
2023-09-04 18:01:11 +0800

    feat: 顶部菜单重做

--- UE-Front-end/vite.config.ts
+++ UE-Front-end/vite.config.ts
@@ -10,7 +10,7 @@ import { resolve } from 'path';
 export default defineConfig({
     server: {
         open: true,
-        // host: '10.219.103.78',
+        host: '10.168.1.104',
     },
     optimizeDeps: {
         include: ['moment', 'echarts']
