7e5975fe3b549ce0332e2c6f6801a07835165df9
wallflower2020 <mr.bes@qq.com>
2023-09-08 09:08:21 +0800

    feat: 中建国际介绍

---
+++ UE-Front-end/src/pages/groupIntro/cschlIntro/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../../components/Title";
+
+const CscHailong: React.FC = () => {
+    return (
+        <div>
+            <Title id="cscHailong" en="CSC HAILONG" />
+        </div>
+    )
+}
+
+export default CscHailong
