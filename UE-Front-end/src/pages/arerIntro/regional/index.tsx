6cc6fbe57470abeadb7923cc3f413603e691f5ed
wallflower99 <emmmbes@gmail.com>
2023-10-24 15:39:56 +0800

    feat: 多余内容删除

--- UE-Front-end/src/pages/arerIntro/regional/index.tsx
+++ UE-Front-end/src/pages/arerIntro/regional/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Panel } from '../../../components/Panel'
 
 const Regional: React.FC<any> = () => {
     return (
         <div>
-            <Panel position='left'>
-                这是区域划分
-            </Panel>
         </div>
     )
 }
