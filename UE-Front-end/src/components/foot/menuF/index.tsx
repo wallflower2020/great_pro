30b2d5f0ec10fffc33394a1e691787866ac7ace5
wallflower99 <emmmbes@gmail.com>
2023-10-26 12:01:14 +0800

    fix: 底部菜单图标更换

--- UE-Front-end/src/components/foot/menuF/index.tsx
+++ UE-Front-end/src/components/foot/menuF/index.tsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router';
 import { callUe4ByMenu } from '../../../util/ue'
 import button1 from '../../../assets/image/button1.png'
 import button2 from '../../../assets/image/button2.png'
+import button3 from '../../../assets/image/button3.png'
+import button4 from '../../../assets/image/button4.png'
+import button5 from '../../../assets/image/button5.png'
+import button6 from '../../../assets/image/button6.png'
 import go from '../../../assets/image/GO.png'
 import ao from '../../../assets/image/AO.png'
 import co from '../../../assets/image/CO.png'
@@ -21,7 +25,7 @@ const MenuF: React.FC<any> = () => {
         navigate('/groupIntro')
     }, [])
 
-    const menuB: Array<string> = [button1, button2]
+    const menuB: Array<string> = [button1, button2, button3, button4, button5, button6]
     const menuEn: Array<string> = [go, ao, co]
 
     const handleClick = (param: any) => {
@@ -46,13 +50,13 @@ const MenuF: React.FC<any> = () => {
 
     const changeColor = (e: any) => {
         if(curr !== Number(e.target.alt)) {
-            e.target.src = menuB[1]
+            e.target.src = menuB[Number(e.target.alt) * 2 + 1]
         }
     }
 
     const changeBack = (e: any) => {
         if(curr !== Number(e.target.alt)) {
-            e.target.src = menuB[0]
+            e.target.src = menuB[Number(e.target.alt) * 2]
         }
     }
 
@@ -62,7 +66,9 @@ const MenuF: React.FC<any> = () => {
                 menus?.map((item, index) => {
                     return (
                         <div className={style.button} style={{ left: `${0.7 + 8.2 * index}vw` }} onClick={() => handleClick(item)} key={item.tag}>
-                            <img src={curr === index ? menuB[1] : menuB[0]} alt={index.toString()} className={style.menuButton} onMouseOver={changeColor} onMouseOut={changeBack}/>
+                            {(index === 0) && <img src={curr === index ? menuB[1] : menuB[0]} alt={index.toString()} className={style.menuButton} onMouseOver={changeColor} onMouseOut={changeBack}/>}
+                            {(index === 1) && <img src={curr === index ? menuB[3] : menuB[2]} alt={index.toString()} className={style.menuButton} onMouseOver={changeColor} onMouseOut={changeBack}/>}
+                            {(index === 2) && <img src={curr === index ? menuB[5] : menuB[4]} alt={index.toString()} className={style.menuButton} onMouseOver={changeColor} onMouseOut={changeBack}/>}
                             <img src={menuEn[index]} className={style.en} />
                             <div className={style.han}>
                                 <Translate id={item.en} />
