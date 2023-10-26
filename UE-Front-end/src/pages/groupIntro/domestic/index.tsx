7dd65d1ab53d24589f919d0b0ec8111fd3feec4a
wallflower99 <emmmbes@gmail.com>
2023-10-26 10:57:15 +0800

    fix: 国内布局基地介绍切换

--- UE-Front-end/src/pages/groupIntro/domestic/index.tsx
+++ UE-Front-end/src/pages/groupIntro/domestic/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../../components/Title";
 import { Panel } from "../../../components/Panel";
 import { PreviewTag } from "../../../components/PreviewTag";
@@ -10,13 +10,17 @@ import board5 from './image/A1B3_5.png'
 import board6 from './image/A1B3_6.png'
 import board7 from './image/A1B3_7.png'
 import board8 from './image/A1B3_8.png'
+import { getUe4Interface } from '../../../util/ue'
 
 const Domestic: React.FC = () => {
 
-    const Board:Array<string> = [board2, board3, board4, board5, board6, board7, board8]
+    const Board:Array<string> = [board3, board8, board4, board6, board5, board7, board2]
 
-    // const [value, setValue] = useState(0)
-    const value = 0
+    const [value, setValue] = useState(6)
+
+    getUe4Interface().Introduce = (param: any) => {
+        setValue(Number(param))
+    }
 
     return (
         <div>
@@ -28,13 +32,19 @@ const Domestic: React.FC = () => {
                     </PreviewTag>
                 </div>
             </Panel>
-            <Panel position="right">
-                <div style={{marginTop: '20.5vh'}}>
-                    <PreviewTag id="right1">
-                        <img src={Board[value]} alt="海龙基地" style={{ width: '17.8vw' }} />
-                    </PreviewTag>
-                </div>
-            </Panel>
+            {
+                Board.map((board, index) => {
+                    return(
+                        <Panel position='right' style={{ display: value === index ? '' : 'none' }} key={index}>
+                            <div style={{marginTop: '20.5vh'}}>
+                                <PreviewTag id={`right${index}`}>
+                                    <img src={board} alt={board} style={{ width: '17.8vw' }}/>
+                                </PreviewTag>
+                            </div>
+                        </Panel>
+                    )
+                })
+            }
         </div>
     )
 }
