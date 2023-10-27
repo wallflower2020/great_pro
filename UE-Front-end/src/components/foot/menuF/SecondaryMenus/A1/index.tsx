49a7d82320a3ab83e093bcb3ee94f930bcb5a36d
wallflower99 <emmmbes@gmail.com>
2023-10-27 09:36:25 +0800

    fix: 远程控制页面轮播逻辑修复

--- UE-Front-end/src/components/foot/menuF/SecondaryMenus/A1/index.tsx
+++ UE-Front-end/src/components/foot/menuF/SecondaryMenus/A1/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './style.module.css'
 import { Slider } from 'antd'
 import slider from '../../../../../assets/image/slider.png'
@@ -10,42 +10,34 @@ import { menus } from '../../menus'
 
 const SecondaryA1: React.FC = () => {
     const [value, setValue] = useState(0)
+    const [flag, setFlag] = useState(-1)
     const navigate = useNavigate()
 
     let auto: any = null
 
     const funcA = () => {
-        console.log(value)
         auto = setTimeout(() => {
-            switch(value){
-                case 0: navigate('/groupIntro/cschlIntro'); break
-                case 1: navigate('/groupIntro/domestic'); break
-                case 2: navigate('/groupIntro/sevenSeries'); break
-                case 3: navigate('/groupIntro/important'); break
-                case 4: navigate('/groupIntro/cscecInter'); break
-            }
-            setValue(value + 1)
-        }, 6000)
+            changeSlider(flag + 1)
+            setFlag(flag + 1)
+        }, 3000)
     }
 
     const funcB = () => {
         clearTimeout(auto)
     }
 
-    // useEffect(() => {
-    //     if(value === 5) {
-    //         setValue(0)
-    //     } else {
-    //         funcA()
-    //     }
-    // }, [value])
+    useEffect(() => {
+        if(flag > -1) {
+            if(flag === 5) {
+                setFlag(0)
+            } else { 
+                funcA()
+            }
+        }
+    }, [flag])
 
     getUe4Interface().change = () => {
-        if(value === 5) {
-            setValue(0)
-        } else {
-            funcA()
-        }
+        setFlag(0)
     }
 
     getUe4Interface().stop = () => {
